1c8f9b1343163f67e1b4b3f23a38b19afb009fcd
bogdan <kazhesman@gmail.com>
2021-12-11 11:30:00 +0200

    pik v2

---
+++ tic.js
@@ -0,0 +1,27 @@
+let tick = true;
+let t;
+function ticker(time) {
+  let d1 = new Date();
+  let dif = (d1.getTime() - d.getTime()) / 1000;
+  if (dif >= time) {
+    clearInterval(t);
+    //return;
+  }
+  if (tick) {
+    document.write("Тик ");
+    tick = false;
+  } else {
+    document.write("Так ");
+    tick = true;
+  }
+  //   count++;
+  //   if (count == 10){
+  //       clearInterval(t);
+  //   }
+}
+
+//let d = new Date();
+//let time;
+t = setInterval(ticker.bind(this, 10), 1000);
+
+
